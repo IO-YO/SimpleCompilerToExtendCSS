fa44a95f1c446297b44aaecc1045dc40534a96a7
Nigel <98767612+AllTriber@users.noreply.github.com>
2025-06-28 21:57:25 +0200

    Refactor Evaluator: implement expression evaluation for literals and addition operations: yayy

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -1,29 +1,51 @@
 package nl.han.ica.icss.transforms;
 
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
+import nl.han.ica.icss.ast.operations.*;
 import nl.han.ica.icss.scoping.ScopeManager;
 
 import static nl.han.ica.icss.scoping.ASTScopeRules.isScopingNode;
 
 public class Evaluator implements Transform {
 
-    ScopeManager<Literal> scopeManager;
-
-    public Evaluator() {
-    }
+    private ScopeManager<Literal> scopeManager;
 
     @Override
     public void apply(AST ast) {
-        scopeManager = new ScopeManager<>();
-        ASTNode sheet = ast.root;
-        doAwesomeTransformation(sheet);
+        this.scopeManager = new ScopeManager<>();
+        transform(ast.root);
     }
 
-    private void doAwesomeTransformation(ASTNode node) {
-        if(isScopingNode(node)) scopeManager.enterScope();
+    private void transform(ASTNode node) {
+        if (isScopingNode(node)) scopeManager.enterScope();
 
-        node.getChildren().forEach(this::doAwesomeTransformation);
-        if(isScopingNode(node)) scopeManager.exitScope();
+        if (node instanceof Declaration decl) handleDeclaration(decl);
+
+        node.getChildren().forEach(this::transform);
+
+        if (isScopingNode(node)) scopeManager.exitScope();
     }
 
+    private void handleDeclaration(Declaration decl) {
+        decl.expression = evaluate(decl.expression);
+    }
+
+    private Literal evaluate(Expression expr) {
+        if (expr instanceof Literal lit) return lit;
+        if (expr instanceof AddOperation op) return evaluateAdd(op);
+        return null;
+    }
+
+    private Literal evaluateAdd(AddOperation op) {
+        Literal lhs = evaluate(op.lhs);
+        Literal rhs = evaluate(op.rhs);
+
+        if (lhs instanceof PixelLiteral l && rhs instanceof PixelLiteral r)
+            return new PixelLiteral(l.value + r.value);
+        if (lhs instanceof PercentageLiteral l && rhs instanceof PercentageLiteral r)
+            return new PercentageLiteral(l.value + r.value);
+
+        return null;
+    }
 }
