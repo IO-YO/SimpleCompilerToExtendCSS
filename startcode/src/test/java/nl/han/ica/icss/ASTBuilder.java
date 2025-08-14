2721607ac0bc52b947f97ef7b90882354badc44f
Nigel <98767612+AllTriber@users.noreply.github.com>
2025-08-14 14:50:44 +0200

    Enhance ASTBuilder and Fixtures: add detailed Javadoc comments for AST node creation methods, improve ifClause and ifClauseWithElse methods, and refactor test cases for clarity and maintainability.

--- startcode/src/test/java/nl/han/ica/icss/ASTBuilder.java
+++ startcode/src/test/java/nl/han/ica/icss/ASTBuilder.java
@@ -4,6 +4,10 @@ import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.BoolLiteral;
 import nl.han.ica.icss.ast.selectors.TagSelector;
 
+/**
+ * Utility class for constructing different types of Abstract Syntax Tree (AST) nodes,
+ * such as stylesheets, style rules, declarations, and conditional clauses.
+ */
 public class ASTBuilder {
 
     public static AST stylesheet(ASTNode... rules) {
@@ -42,6 +46,18 @@ public class ASTBuilder {
         return decl;
     }
 
+    /**
+     * Creates an {@code IfClause} object by adding a conditional expression
+     * and a series of body nodes representing the execution logic when the
+     * condition evaluates to true.
+     *
+     * @param bool the conditional {@code ASTNode} that determines whether
+     *             the body nodes will be executed. Typically an {@code Expression}.
+     * @param bodyNodes an array of {@code ASTNode} objects representing the body
+     *                  to be executed when the condition is true.
+     * @return an {@code IfClause} object containing the conditional expression
+     *         and the specified body nodes.
+     */
     public static IfClause ifClause(ASTNode bool, ASTNode... bodyNodes) {
         IfClause clause = new IfClause();
         clause.addChild(bool);
@@ -51,23 +67,76 @@ public class ASTBuilder {
         return clause;
     }
 
-    public static IfClause ifClauseWithElse(ASTNode condition, ASTNode ifBody, ASTNode... elseBodyNodes) {
-        if (!(condition instanceof BoolLiteral || condition instanceof VariableReference))
+    /**
+     * Creates an {@code IfClause} object with a condition, a list of nodes to execute if the condition is true,
+     * and a list of nodes to execute if the condition is false.
+     *
+     * @param condition the conditional expression for the {@code IfClause}. This must be a boolean literal
+     *                  or a variable reference; otherwise, an {@code IllegalArgumentException} will be thrown.
+     * @param ifBodyNodes an array of {@code ASTNode} objects representing the body to execute when the condition is true.
+     *                    Can be {@code null}, in which case no nodes are added for the "if" branch.
+     * @param elseBodyNodes an array of {@code ASTNode} objects representing the body to execute when the condition is false.
+     *                      Can be {@code null}, in which case no nodes are added for the "else" branch.
+     * @return an {@code IfClause} object containing the condition, "if" body nodes, and optional "else" body nodes.
+     * @throws IllegalArgumentException if the condition is not a boolean literal or a variable reference.
+     */
+    public static IfClause ifClauseWithElse(ASTNode condition,
+                                            ASTNode[] ifBodyNodes,
+                                            ASTNode[] elseBodyNodes) {
+        if (!(condition instanceof BoolLiteral || condition instanceof VariableReference)) {
             throw new IllegalArgumentException("Condition must be a boolean or variable reference");
-
-        ElseClause elseClause = new ElseClause();
-        for (ASTNode node : elseBodyNodes) {
-            elseClause.addChild(node);
         }
+
         IfClause clause = new IfClause();
         clause.addChild(condition);
-        if (ifBody != null) {
-            clause.addChild(ifBody);
+
+        if (ifBodyNodes != null) {
+            for (ASTNode node : ifBodyNodes) {
+                clause.addChild(node);
+            }
         }
+
+        ElseClause elseClause = new ElseClause();
+        if (elseBodyNodes != null) {
+            for (ASTNode node : elseBodyNodes) {
+                elseClause.addChild(node);
+            }
+        }
+
         clause.addChild(elseClause);
         return clause;
     }
 
+    /**
+     * Creates an {@code IfClause} object with a condition, a single node to execute if the condition is true,
+     * and a list of nodes to execute if the condition is false.
+     *
+     * @param condition the conditional expression for the {@code IfClause}. This must be a boolean literal
+     *                  or a variable reference; otherwise, an {@code IllegalArgumentException} will be thrown.
+     * @param ifBody a single {@code ASTNode} representing the body to execute when the condition is true.
+     *               Can be {@code null}, in which case no nodes are added for the "if" branch.
+     * @param elseBodyNodes an array of {@code ASTNode} objects representing the body to execute when the condition is false.
+     *                      Can be {@code null}, in which case no nodes are added for the "else" branch.
+     * @return an {@code IfClause} object containing the condition, "if" body node, and optional "else" body nodes.
+     * @throws IllegalArgumentException if the condition is not a boolean literal or a variable reference.
+     */
+    public static IfClause ifClauseWithElse(ASTNode condition,
+                                            ASTNode ifBody,
+                                            ASTNode... elseBodyNodes) {
+        ASTNode[] ifArr = (ifBody == null) ? new ASTNode[0] : new ASTNode[]{ ifBody };
+        ASTNode[] elseArr = (elseBodyNodes == null) ? new ASTNode[0] : elseBodyNodes;
+        return ifClauseWithElse(condition, ifArr, elseArr);
+    }
+
+    /**
+     * Creates an {@code AST} object with a single style rule containing one property declaration.
+     * The rule is identified by a tag name, and the property declaration consists of a property name and its corresponding expression value.
+     *
+     * @param ruleTag the name of the tag that represents the rule. Must not be null.
+     * @param propertyName the name of the property to be declared in the style rule. Must not be null.
+     * @param expression the value of the property, represented as an {@code Expression} object. Must not be null.
+     * @return an {@code AST} object representing the stylesheet containing the specified rule and property declaration.
+     */
     public static AST ruleWithPropertyDeclaration(String ruleTag, String propertyName, Expression expression) {
         return ASTBuilder.stylesheet(
                 ASTBuilder.rule(ruleTag,
--- startcode/src/test/java/nl/han/ica/icss/transforms/Fixtures.java
+++ startcode/src/test/java/nl/han/ica/icss/transforms/Fixtures.java
@@ -2,8 +2,6 @@ package nl.han.ica.icss.transforms;
 
 import nl.han.ica.icss.ASTBuilder;
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.literals.BoolLiteral;
-import nl.han.ica.icss.ast.literals.PixelLiteral;
 
 public class Fixtures {
 
@@ -24,221 +22,36 @@ public class Fixtures {
         );
 
         return new ASTPair(input, expected);
+
     }
 
-    public static ASTPair createIfWidthPair(
-            Expression condition,
-            Literal thenWidth,
-            Literal elseWidthOrNull,
-            Literal expectedWidthOrNull
-    ) {
-        // Build the if-body
-        ASTNode thenDecl = ASTBuilder.decl("width", thenWidth);
-
-        // Input: stylesheet([prelude...], rule(p { if [cond] { width: then } else { width: else? } }))
+    public static ASTPair createIfPair(Expression condition,
+                                       ASTNode[] ifBody,
+                                       ASTNode[] expectedBody) {
         AST input = ASTBuilder.stylesheet(
                 ASTBuilder.rule("p",
-                        elseWidthOrNull == null
-                                ? ASTBuilder.ifClause(condition, thenDecl)
-                                : ASTBuilder.ifClauseWithElse(
-                                condition,
-                                thenDecl,
-                                ASTBuilder.decl("width", elseWidthOrNull))
+                        ASTBuilder.ifClause(condition, ifBody)
                 )
         );
-
-        // Expected: stylesheet(rule(p { width: expected? }))
-        AST expected = (expectedWidthOrNull == null)
-                ? ASTBuilder.stylesheet(ASTBuilder.rule("p"))
-                : ASTBuilder.stylesheet(
-                ASTBuilder.rule("p", ASTBuilder.decl("width", expectedWidthOrNull))
+        AST expected = ASTBuilder.stylesheet(
+                ASTBuilder.rule("p", expectedBody)
         );
-
         return new ASTPair(input, expected);
     }
 
-    // --- NEW: helper to quickly make `X := <bool>` assignments for prelude ---
-    public static VariableAssignment assignBool(String name, boolean value) {
-        return ASTBuilder.assign(name, new BoolLiteral(value));
-    }
-
-    private static AST createASTPairForIfElseClauses(
-            Expression condition,
-            Declaration... ifBody
-    ) {
+    public static ASTPair createIfElsePair(Expression condition,
+                                           ASTNode[] ifBody,
+                                           ASTNode[] elseBody,
+                                           ASTNode[] expectedBody) {
         AST input = ASTBuilder.stylesheet(
                 ASTBuilder.rule("p",
-                        ASTBuilder.ifClauseWithElse(
-                                condition,
-                                ifBody
-                        )
+                        ASTBuilder.ifClauseWithElse(condition, ifBody, elseBody)
                 )
         );
-
         AST expected = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ifBody, // if body is always kept
-                        elseBody // else body is always kept
-                )
+                ASTBuilder.rule("p", expectedBody)
         );
-
         return new ASTPair(input, expected);
-    })
-
-    private static AST ifClause_Simple(boolean condition) {
-        return ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.ifClause(
-                                new BoolLiteral(condition),
-                                ASTBuilder.decl(
-                                        "width",
-                                        new PixelLiteral(10)
-                                )
-                        )
-                )
-        );
-    }
-
-
-    public static ASTPair ifClause_TrueCase() {
-        AST input = ifClause_Simple(true);
-
-        AST expected = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.decl("width", new PixelLiteral(10))
-                )
-        );
-
-        return new ASTPair(input, expected);
-    }
-    public static ASTPair ifClause_FalseCase() {
-        AST input = ifClause_Simple(false);
-
-        AST expected = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p")
-        );
-
-        return new ASTPair(input, expected);
-    }
-
-
-    public static AST ifClause_VariableAssignment_Simple(boolean bool) {
-        return ASTBuilder.stylesheet(
-                ASTBuilder.assign("c", new BoolLiteral(bool)),
-                ASTBuilder.rule("p",
-                        ASTBuilder.ifClause(
-                                new VariableReference("c"),
-                                ASTBuilder.decl(
-                                        "width",
-                                        new PixelLiteral(10)
-                                )
-                        )
-                )
-        );
-
-    }
-
-    public static ASTPair ifClause_VariableAssignment_TrueCase() {
-        AST input = ifClause_VariableAssignment_Simple(true);
-
-        AST expected = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.decl("width", new PixelLiteral(10))
-                )
-        );
-
-        return new ASTPair(input, expected);
-    }
-
-    public static ASTPair ifClause_VariableAssignment_FalseCase() {
-        AST input = ifClause_VariableAssignment_Simple(false);
-
-        AST expected = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p")
-        );
-
-        return new ASTPair(input, expected);
-    }
-
-
-    public static ASTPair variableTransform_globalVar_refToRuleBodyDecl() {
-
-        AST input = ASTBuilder.stylesheet(
-                ASTBuilder.assign("DefaultWidth", new PixelLiteral(42)),
-                ASTBuilder.rule("p",
-                        ASTBuilder.declVar("width", "DefaultWidth")
-                )
-        );
-
-        AST expected = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.decl("width", new PixelLiteral(42))
-                )
-        );
-
-        return new ASTPair(input, expected);
-    }
-
-    public static ASTPair variableTransform_scopedVarInStyleRule_refToLiteral() {
-        AST input = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.assign("WidthVar", new PixelLiteral(10)),
-                        ASTBuilder.declVar("width", "WidthVar")
-                )
-        );
-
-        AST expected = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.decl("width", new PixelLiteral(10))
-                )
-        );
-
-        return new ASTPair(input, expected);
-    }
-
-    private static AST ifWithElse(ASTNode condition) {
-        return ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.ifClauseWithElse(
-                                condition, // condition node
-                                ASTBuilder.decl("color", new PixelLiteral(1)), // if body
-                                ASTBuilder.decl("color", new PixelLiteral(2)) // else body
-                        )
-                )
-        );
-    }
-
-    public static ASTPair ifWithElse_ifFalse_keepsElse() {
-        AST in  = ifWithElse(new BoolLiteral(false));
-        AST exp = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.decl("color", new PixelLiteral(2))   // else-body kept
-                )
-        );
-        return new ASTPair(in, exp);
-    }
-
-    public static ASTPair ifIfElse_outerTrueKeepsInnerIf_innerFalseKeepsElse() {
-        AST in = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.ifClause(
-                                new BoolLiteral(true),
-                                ASTBuilder.ifClauseWithElse(
-                                        new BoolLiteral(false), // inner condition
-                                        ASTBuilder.decl("width", new PixelLiteral(1)),
-                                        ASTBuilder.decl("width", new PixelLiteral(9))
-                                )
-                        )
-                )
-        );
-
-        AST exp = ASTBuilder.stylesheet(
-                ASTBuilder.rule("p",
-                        ASTBuilder.decl("width", new PixelLiteral(9))   // deepest else
-                )
-        );
-
-        return new ASTPair(in, exp);
     }
 
 }
