4f3e3e1b2752d08d4c7b4b8e873074c2afc2999c
Nigel <98767612+AllTriber@users.noreply.github.com>
2025-08-19 21:56:24 +0200

    :')

--- startcode/src/test/java/nl/han/ica/icss/transforms/EvaluatorTest.java
+++ startcode/src/test/java/nl/han/ica/icss/transforms/EvaluatorTest.java
@@ -16,8 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EvaluatorTest {
 
-    public record EvalCase(String name, Expression input, Literal expected) {
-    }
+    public record EvalCase(String name, Expression input, Literal expected) {}
 
     private void assertEvaluatedCorrectly(EvaluatorTestBuilder.ASTPair test) {
         new Evaluator().apply(test.input());
@@ -26,10 +25,26 @@ class EvaluatorTest {
 
     static Stream<Arguments> ExpressionAdditionCases() {
         return Stream.of(
-                Arguments.of(new EvalCase("Add Pixels", addition(px(1), px(1)), px(2))),
-                Arguments.of(new EvalCase("Add Percentages", addition(percent(10), percent(20)), percent(30))),
-                Arguments.of(new EvalCase("Add Scalars", addition(scalar(10), scalar(20)), scalar(30))),
-                Arguments.of(new EvalCase("Add MultiplePixels", addition(addition(px(5), px(10)), px(15)), px(30)))
+                Arguments.of(new EvalCase(
+                        "Add Pixels",
+                        addition(px(1), px(1)),
+                        px(2)
+                )),
+                Arguments.of(new EvalCase(
+                        "Add Percentages",
+                        addition(percent(10), percent(20)),
+                        percent(30)
+                )),
+                Arguments.of(new EvalCase(
+                        "Add Scalars",
+                        addition(scalar(10), scalar(20)),
+                        scalar(30)
+                )),
+                Arguments.of(new EvalCase(
+                        "Add Multiple Pixels",
+                        addition(addition(px(5), px(10)), px(15)),
+                        px(30)
+                ))
         );
     }
 
@@ -40,18 +55,42 @@ class EvaluatorTest {
     void TR01_Addition_EvaluatesCorrectly(EvalCase testCase) {
         assertEvaluatedCorrectly(
                 EvaluatorTestBuilder.build()
-                        .input(rule("p", decl("width", testCase.input())))
-                        .expected(rule("p", decl("width", testCase.expected())))
+                        .input(
+                                rule("p",
+                                        decl("width", testCase.input())
+                                )
+                        )
+                        .expected(
+                                rule("p",
+                                        decl("width", testCase.expected())
+                                )
+                        )
                         .toPair()
         );
     }
 
     static Stream<Arguments> ExpressionSubtractionCases() {
         return Stream.of(
-                Arguments.of(new EvalCase("Subtract Pixels", subtract(px(10), px(5)), px(5))),
-                Arguments.of(new EvalCase("Subtract Percentages", subtract(percent(50), percent(20)), percent(30))),
-                Arguments.of(new EvalCase("Subtract Scalars", subtract(scalar(100), scalar(105)), scalar(-5))),
-                Arguments.of(new EvalCase("Subtract MultiplePixels", subtract(subtract(px(50), px(20)), px(10)), px(20)))
+                Arguments.of(new EvalCase(
+                        "Subtract Pixels",
+                        subtract(px(10), px(5)),
+                        px(5)
+                )),
+                Arguments.of(new EvalCase(
+                        "Subtract Percentages",
+                        subtract(percent(50), percent(20)),
+                        percent(30)
+                )),
+                Arguments.of(new EvalCase(
+                        "Subtract Scalars",
+                        subtract(scalar(100), scalar(105)),
+                        scalar(-5)
+                )),
+                Arguments.of(new EvalCase(
+                        "Subtract Multiple Pixels",
+                        subtract(subtract(px(50), px(20)), px(10)),
+                        px(20)
+                ))
         );
     }
 
@@ -62,18 +101,42 @@ class EvaluatorTest {
     void TR01_Subtraction_EvaluatesCorrectly(EvalCase testCase) {
         assertEvaluatedCorrectly(
                 EvaluatorTestBuilder.build()
-                        .input(rule("p", decl("width", testCase.input())))
-                        .expected(rule("p", decl("width", testCase.expected())))
+                        .input(
+                                rule("p",
+                                        decl("width", testCase.input())
+                                )
+                        )
+                        .expected(
+                                rule("p",
+                                        decl("width", testCase.expected())
+                                )
+                        )
                         .toPair()
         );
     }
 
     static Stream<Arguments> ExpressionSubtractAndAdditionCases() {
         return Stream.of(
-                Arguments.of(new EvalCase("Subtract And Add Pixels", addition(subtract(px(20), px(10)), px(5)), px(15))),
-                Arguments.of(new EvalCase("Subtract And Add Percentages", addition(subtract(percent(30), percent(10)), percent(5)), percent(25))),
-                Arguments.of(new EvalCase("Subtract And Add Scalars", addition(subtract(scalar(50), scalar(20)), scalar(10)), scalar(40))),
-                Arguments.of(new EvalCase("Subtract And Add Multiple Pixels", addition(subtract(addition(px(30), px(20)), px(10)), px(5)), px(45)))
+                Arguments.of(new EvalCase(
+                        "Subtract And Add Pixels",
+                        addition(subtract(px(20), px(10)), px(5)),
+                        px(15)
+                )),
+                Arguments.of(new EvalCase(
+                        "Subtract And Add Percentages",
+                        addition(subtract(percent(30), percent(10)), percent(5)),
+                        percent(25)
+                )),
+                Arguments.of(new EvalCase(
+                        "Subtract And Add Scalars",
+                        addition(subtract(scalar(50), scalar(20)), scalar(10)),
+                        scalar(40)
+                )),
+                Arguments.of(new EvalCase(
+                        "Subtract And Add Multiple Pixels",
+                        addition(subtract(addition(px(30), px(20)), px(10)), px(5)),
+                        px(45)
+                ))
         );
     }
 
@@ -84,20 +147,52 @@ class EvaluatorTest {
     void TR01_SubtractAndAdd_EvaluatesCorrectly(EvalCase testCase) {
         assertEvaluatedCorrectly(
                 EvaluatorTestBuilder.build()
-                        .input(rule("p", decl("width", testCase.input())))
-                        .expected(rule("p", decl("width", testCase.expected())))
+                        .input(
+                                rule("p",
+                                        decl("width", testCase.input())
+                                )
+                        )
+                        .expected(
+                                rule("p",
+                                        decl("width", testCase.expected())
+                                )
+                        )
                         .toPair()
         );
     }
 
     static Stream<Arguments> ExpressionMultiplicationCases() {
         return Stream.of(
-                Arguments.of(new EvalCase("Multiply Scalar", multiply(scalar(4), scalar(5)), scalar(20))),
-                Arguments.of(new EvalCase("Multiply Multiple Pixels", multiply(multiply(scalar(2), px(3)), scalar(4)), px(24))),
-                Arguments.of(new EvalCase("Multiply Scalar And Percentage", multiply(scalar(4), percent(5)), percent(20))),
-                Arguments.of(new EvalCase("Multiply Percentage And Scalar", multiply(percent(4), scalar(5)), percent(20))),
-                Arguments.of(new EvalCase("Multiply Pixel And Scalar", multiply(px(10), scalar(2)), px(20))),
-                Arguments.of(new EvalCase("Multiply Scalar And Pixel", multiply(scalar(2), px(10)), px(20)))
+                Arguments.of(new EvalCase(
+                        "Multiply Scalar",
+                        multiply(scalar(4), scalar(5)),
+                        scalar(20)
+                )),
+                Arguments.of(new EvalCase(
+                        "Multiply Multiple Pixels",
+                        multiply(multiply(scalar(2), px(3)), scalar(4)),
+                        px(24)
+                )),
+                Arguments.of(new EvalCase(
+                        "Multiply Scalar And Percentage",
+                        multiply(scalar(4), percent(5)),
+                        percent(20)
+                )),
+                Arguments.of(new EvalCase(
+                        "Multiply Percentage And Scalar",
+                        multiply(percent(4), scalar(5)),
+                        percent(20)
+                )),
+                Arguments.of(new EvalCase(
+                        "Multiply Pixel And Scalar",
+                        multiply(px(10), scalar(2)),
+                        px(20)
+                )),
+                Arguments.of(new EvalCase(
+                        "Multiply Scalar And Pixel",
+                        multiply(scalar(2), px(10)),
+                        px(20)
+                ))
         );
     }
 
@@ -108,22 +203,48 @@ class EvaluatorTest {
     void TR01_Multiplication_EvaluatesCorrectly(EvalCase testCase) {
         assertEvaluatedCorrectly(
                 EvaluatorTestBuilder.build()
-                        .input(rule("p", decl("width", testCase.input())))
-                        .expected(rule("p", decl("width", testCase.expected())))
+                        .input(
+                                rule("p",
+                                        decl("width", testCase.input())
+                                )
+                        )
+                        .expected(
+                                rule("p",
+                                        decl("width", testCase.expected())
+                                )
+                        )
                         .toPair()
         );
     }
 
     static Stream<Arguments> MixedOperationsCases() {
         return Stream.of(
-                Arguments.of(new EvalCase("Mixed Add And Multiply", addition(multiply(px(2), scalar(3)), px(4)),
-                        px(10))),
-                Arguments.of(new EvalCase("Mixed Subtract And Multiply", subtract(multiply(percent(5), scalar(2)), percent(3)),
-                        percent(7))),
-                Arguments.of(new EvalCase("Mixed Add Subtract And Multiply", addition(subtract(scalar(10), scalar(2)), multiply(scalar(2), scalar(3))),
-                        scalar(14))),
-                Arguments.of(new EvalCase("Mixed Add Subtract And Multiply Pixels", addition(subtract(px(10), px(2)), multiply(scalar(2), px(3))),
-                        px(14)))
+                Arguments.of(new EvalCase(
+                        "Mixed Add And Multiply",
+                        addition(multiply(px(2), scalar(3)), px(4)),
+                        px(10)
+                )),
+                Arguments.of(new EvalCase(
+                        "Mixed Subtract And Multiply",
+                        subtract(multiply(percent(5), scalar(2)), percent(3)),
+                        percent(7)
+                )),
+                Arguments.of(new EvalCase(
+                        "Mixed Add Subtract And Multiply",
+                        addition(
+                                subtract(scalar(10), scalar(2)),
+                                multiply(scalar(2), scalar(3))
+                        ),
+                        scalar(14)
+                )),
+                Arguments.of(new EvalCase(
+                        "Mixed Add Subtract And Multiply Pixels",
+                        addition(
+                                subtract(px(10), px(2)),
+                                multiply(scalar(2), px(3))
+                        ),
+                        px(14)
+                ))
         );
     }
 
@@ -134,8 +255,16 @@ class EvaluatorTest {
     void TR01_MixedOperations_EvaluatesCorrectly(EvalCase testCase) {
         assertEvaluatedCorrectly(
                 EvaluatorTestBuilder.build()
-                        .input(rule("p", decl("width", testCase.input())))
-                        .expected(rule("p", decl("width", testCase.expected())))
+                        .input(
+                                rule("p",
+                                        decl("width", testCase.input())
+                                )
+                        )
+                        .expected(
+                                rule("p",
+                                        decl("width", testCase.expected())
+                                )
+                        )
                         .toPair()
         );
     }
@@ -149,45 +278,77 @@ class EvaluatorTest {
 
     static Stream<Arguments> IfElseCases() {
         return Stream.of(
-                Arguments.of(new EvalConditionalRuleCase("a: If True",
+                Arguments.of(new EvalConditionalRuleCase(
+                        "a: If(True)",
                         () -> EvaluatorTestBuilder.build()
                                 .input(rule("p", ifClause(bool(true), decl("width", px(10)))))
                                 .expected(rule("p", decl("width", px(10))))
                                 .toPair()
                 )),
-                Arguments.of(new EvalConditionalRuleCase("a: If False",
+                Arguments.of(new EvalConditionalRuleCase(
+                        "a: If(False)",
                         () -> EvaluatorTestBuilder.build()
                                 .input(rule("p", ifClause(bool(false), decl("width", px(10)))))
                                 .expected(rule("p"))
                                 .toPair()
                 )),
-                Arguments.of(new EvalConditionalRuleCase("b: If-Else True",
+                Arguments.of(new EvalConditionalRuleCase(
+                        "b: If(true) no Else",
                         () -> EvaluatorTestBuilder.build()
-                                .input(rule("p",
-                                        ifElseClause(
-                                                bool(true),
-                                                new ASTNode[]{decl("width", px(10))},
-                                                new ASTNode[]{decl("width", px(20))}))
+                                .input(
+                                        rule("p",
+                                                ifElseClause(
+                                                        bool(true),
+                                                        new ASTNode[]{decl("width", px(10))},
+                                                        new ASTNode[]{decl("width", px(20))}
+                                                )
+                                        )
                                 )
                                 .expected(rule("p", decl("width", px(10))))
                                 .toPair()
                 )),
-                Arguments.of(new EvalConditionalRuleCase("b: If-Else False",
+                Arguments.of(new EvalConditionalRuleCase(
+                        "b: If(false) then Else",
                         () -> EvaluatorTestBuilder.build()
                                 .input(rule("p", ifElseClause(bool(false), decl("width", px(10)), decl("width", px(20)))))
                                 .expected(rule("p", decl("width", px(20))))
                                 .toPair()
                 )),
-                Arguments.of(new EvalConditionalRuleCase("c: If True with Prefix and Suffix",
+                Arguments.of(new EvalConditionalRuleCase(
+                        "a: If(True) with Prefix and Suffix",
                         () -> EvaluatorTestBuilder.build()
-                                .input(rule("p", decl("margin", px(20)), ifClause(bool(true), decl("width", px(10))), decl("padding", px(30))))
-                                .expected(rule("p", decl("margin", px(20)), decl("width", px(10)), decl("padding", px(30))))
+                                .input(
+                                        rule("p",
+                                                decl("margin", px(20)),
+                                                ifClause(bool(true), decl("width", px(10))),
+                                                decl("padding", px(30))
+                                        )
+                                )
+                                .expected(
+                                        rule("p",
+                                                decl("margin", px(20)),
+                                                decl("width", px(10)),
+                                                decl("padding", px(30))
+                                        )
+                                )
                                 .toPair()
                 )),
-                Arguments.of(new EvalConditionalRuleCase("d: If False with Prefix and Suffix",
+                Arguments.of(new EvalConditionalRuleCase(
+                        "a: If(False) with Prefix and Suffix",
                         () -> EvaluatorTestBuilder.build()
-                                .input(rule("p", decl("margin", px(20)), ifClause(bool(false), decl("width", px(10))), decl("padding", px(30))))
-                                .expected(rule("p", decl("margin", px(20)), decl("padding", px(30))))
+                                .input(
+                                        rule("p",
+                                                decl("margin", px(20)),
+                                                ifClause(bool(false), decl("width", px(10))),
+                                                decl("padding", px(30))
+                                        )
+                                )
+                                .expected(
+                                        rule("p",
+                                                decl("margin", px(20)),
+                                                decl("padding", px(30))
+                                        )
+                                )
                                 .toPair()
                 ))
         );
